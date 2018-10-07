df89a3b00309c21a2574c2cc78be7608efd9f34a
Rafael Bardini <rafael@rbardini.com>
2018-10-07 13:03:24 +0200

    Update dependencies
    - Use exact versions
    - Remove unused imports and dependencies
    - Remove all themes but `even`
    - Replace `open` with `opn` due to npm security advisory 663
    - Move `should` to devDependencies
    - Remove deprecated `preferGlobal` option
    - Drop support for Node 6
    - Add package-lock.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('dotenv').load({silent: true});
+require('dotenv').load();
 var pkg = require(__dirname + '/package.json');
 var lib = require(__dirname + '/lib');
 var program = require('commander');
